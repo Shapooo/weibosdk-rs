5338caf362ec324f6a527587937d12e13396c250
Shapooo <ShaboJia@outlook.com>
2025-07-08 12:43:38 +0800

    为 LoginError 添加 Error 和 Display 实现

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum LoginError {
     SessionNotFound,
@@ -5,9 +7,27 @@ pub enum LoginError {
     NetworkError(anyhow::Error),
 }
 
+impl fmt::Display for LoginError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LoginError::SessionNotFound => write!(f, "Session not found"),
+            LoginError::SessionInvalid => write!(f, "Session invalid"),
+            LoginError::NetworkError(err) => write!(f, "Network error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for LoginError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            LoginError::NetworkError(err) => Some(err.as_ref()),
+            _ => None,
+        }
+    }
+}
+
 impl From<anyhow::Error> for LoginError {
     fn from(err: anyhow::Error) -> Self {
         LoginError::NetworkError(err)
     }
-    // ... other errors
 }
