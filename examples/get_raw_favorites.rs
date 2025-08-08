83c4bb3f5e59f11dc994b1d2bf99ec16ae8d3c84
Shapooo <ShaboJia@outlook.com>
2025-08-08 11:17:10 +0800

    去掉不必要的依赖，使用大版本依赖

--- examples/get_raw_favorites.rs
+++ examples/get_raw_favorites.rs
@@ -3,7 +3,7 @@ use std::io::{self, Write};
 use weibosdk_rs::{favorites::FavoritesAPI, session::Session, weibo_api::WeiboAPIImpl};
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() {
     simple_logger::init_with_level(log::Level::Debug).unwrap();
     let session_file = "session.json";
     let client = weibosdk_rs::client::new_client_with_headers().unwrap();
@@ -11,29 +11,27 @@ async fn main() -> anyhow::Result<()> {
 
     if let Ok(session) = Session::load(session_file) {
         println!("Loaded session from {}", session_file);
-        weibo_api.login_with_session(session).await?;
+        weibo_api.login_with_session(session).await.unwrap();
     } else {
         println!("No session file found. Starting new login.");
 
         print!("Please enter your phone number: ");
-        io::stdout().flush()?;
+        io::stdout().flush().unwrap();
         let mut phone_number = String::new();
-        io::stdin().read_line(&mut phone_number)?;
+        io::stdin().read_line(&mut phone_number).unwrap();
 
-        weibo_api.get_sms_code(phone_number).await?;
+        weibo_api.get_sms_code(phone_number).await.unwrap();
 
         print!("Please enter the SMS code: ");
-        io::stdout().flush()?;
+        io::stdout().flush().unwrap();
         let mut sms_code = String::new();
-        io::stdin().read_line(&mut sms_code)?;
+        io::stdin().read_line(&mut sms_code).unwrap();
 
-        weibo_api.login(sms_code.trim()).await?;
+        weibo_api.login(sms_code.trim()).await.unwrap();
     };
 
-    let session = weibo_api.session()?;
-    session.save(session_file)?;
+    let session = weibo_api.session().unwrap();
+    session.save(session_file).unwrap();
     println!("Session saved to {}", session_file);
-    let _favorites = weibo_api.favorites(1).await?;
-
-    Ok(())
+    let _favorites = weibo_api.favorites(1).await.unwrap();
 }
